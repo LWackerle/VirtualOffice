b37c1e62f7d184f7aeb59d66d7337d9968b14ed6
Lorenz J. C. Wackerle <lorenzwackerle@gmail.com>
2021-03-20 17:21:17 +0100

    Edited Tsx to remove wrong path

--- palette_interiors_x3.tsx
+++ palette_interiors_x3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="palette_interiors_x3" tilewidth="32" tileheight="32" tilecount="4" columns="1">
- <image source="../../../../../../../Projects/Telekom/Learning Company/Modern_Interiors/Palettes/palette_interiors_x3.png" width="63" height="135"/>
+ <image source="palette_interiors_x3.png" width="63" height="135"/>
 </tileset>
