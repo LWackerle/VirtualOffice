c517f5f59b5197794f59c8f113114bc074770bda
Lorenz J. C. Wackerle <lorenzwackerle@gmail.com>
2021-03-20 17:23:37 +0100

    removed further paths

--- Room_Builder_Floor_Paths_32x32.tsx
+++ Room_Builder_Floor_Paths_32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Room_Builder_Floor_Paths_32x32" tilewidth="32" tileheight="32" tilecount="504" columns="42">
- <image source="../../../../../../../Projects/Telekom/Learning Company/Modern_Interiors/1_Interiors/32x32/Room_Bulder_subfiles_32x32/Room_Builder_Floor_Paths_32x32.png" width="1344" height="384"/>
+ <image source="Room_Builder_Floor_Paths_32x32.png" width="1344" height="384"/>
 </tileset>
